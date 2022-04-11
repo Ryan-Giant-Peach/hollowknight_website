e73ce285bce3924fd94abc66ccb544ebe6095a16
Ryan Caulfield <Ryancaulfield3103@gmail.com>
2022-04-11 22:44:37 +0100

    first commit of hollowknight website for React practice

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Vidorslides from './components/Vidorslides';
+import Info from './components/Info';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Heyooo</h1>
+      <Vidorslides />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Vidorslides.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Vidorslides() {
+  return (
+    <div>vidorslides</div>
+  )
+}
+
+export default Vidorslides
